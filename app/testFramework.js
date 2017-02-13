14204b2d53743b5a0b63aa7ae2646e8dd5b19306
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2017-02-13 13:45:29 +0000

    update

--- app/testFramework.js
+++ app/testFramework.js
@@ -1,9 +1,16 @@
 function test(){}
 
 test.Equals = function(passFunction, result, answer="expected " + passFunction + " to equal " + result) {
-  if (passFunction===result){return true}else{return answer}
-}
+  if (passFunction===result){return true}else{return answer};
+};
 
 test.NotEquals = function(passFunction, result, answer="expected " + passFunction + " not to equal " + result) {
-  if (passFunction!==result){return true}else{return answer}
-}
+  if (passFunction!==result){return true}else{return answer};
+};
+
+test.HasContent = function(webpage, result, answer="FALSE") {
+
+};
+
+console.log(document.URL)
+console.log(document.body)
--- spec/specfile.js
+++ spec/specfile.js
@@ -1,2 +1,2 @@
 console.log(test.Equals(returnTrue(),"HELLO"))
-console.log(test.NotEquals(returnTrue(),4))
+console.log(test.NotEquals(returnTrue(),1,"THIS IS WHAT I WANT TO DISPLAY"))
