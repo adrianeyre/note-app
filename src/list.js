f41c9756cf598ad315321e6b5c49a8366f7d6fb6
James Turner <mail.jt@gmail.com>
2017-02-13 13:51:21 +0000

    Update list.js
    edited to use an array as discussed

--- src/list.js
+++ src/list.js
@@ -1,12 +1,10 @@
 'use strict';
 
 function List() {
-  this.notes = {};
-  this.id = 0;
+  this.notes = [];
 }
 
 List.prototype.createNote = function(description) {
-  this.id += 1;
-  var note = new Note(description, this.id);
-  this.notes[note.id] = note;
+  var note = new Note(description);
+  this.notes.push(note);
 };
