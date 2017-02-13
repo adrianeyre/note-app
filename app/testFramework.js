60d45b1d73d0df13bfc98dac00b7db19d71dc73c
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2017-02-13 15:26:44 +0000

    Framework now in it block

--- app/testFramework.js
+++ app/testFramework.js
@@ -1,21 +1,30 @@
 function test(){}
 
 test.Equals = function(passFunction, result, answer="expected " + passFunction + " to equal " + result) {
-  if (passFunction===result){return true}else{return answer}
+  if (passFunction===result){return true}else{return answer};
 };
 
 test.NotEquals = function(passFunction, result, answer="expected " + passFunction + " not to equal " + result) {
-  if (passFunction!==result){return true}else{return answer}
+  if (passFunction!==result){return true}else{return answer};
 };
 
 test.GreaterThan = function(passFunction, result, answer="expected " + passFunction + " to be greater than " + result) {
-  if (passFunction > result){return true}else{return answer}
+  if (passFunction > result){return true}else{return answer};
 };
 
 test.LessThan = function(passFunction, result, answer="expected " + passFunction + " to be less than " + result) {
-  if (passFunction < result){return true}else{return answer}
+  if (passFunction < result){return true}else{return answer};
 };
 
 test.Contains = function(passFunction, result, answer="expected " + passFunction + " to contain " + result) {
-  if (passFunction.includes(result)){return true}else{return answer}
+  if (passFunction.includes(result)){return true}else{return answer};
+};
+
+
+this.it = function(title, passFunction){
+  var result = passFunction();
+  var css = "";
+  if (result!==true){css="in"};
+  var output = "<div id='"+css+"correct'>"+title+": "+result+"</div>";
+  document.write(output);
 };
--- spec/testSpec.js
+++ spec/testSpec.js
@@ -1,4 +1,16 @@
-console.log(test.Equals(returnTrue(),"HELLO"));
-console.log(test.NotEquals(returnTrue(),1,"THIS IS WHAT I WANT TO DISPLAY"));
-console.log(test.GreaterThan(greaterThan(), 6));
-console.log(test.Contains(array(),"bean"));
+// console.log(test.Equals(returnTrue(),"HELLO"));
+// console.log(test.NotEquals(returnTrue(),1,"THIS IS WHAT I WANT TO DISPLAY"));
+// console.log(test.GreaterThan(greaterThan(), 6));
+// console.log(test.Contains(array(),"bean"));
+
+it('returns HELLO', function(){
+  return test.Equals(returnTrue(),"HELLO");
+});
+
+it('returns 1', function(){
+  return test.Equals(returnTrue(),1);
+});
+
+it('array contains element bean', function(){
+  return test.Contains(array(),"bean");
+});
