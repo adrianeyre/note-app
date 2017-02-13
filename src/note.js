b0d8cc726dd669f49747f02c140268058b7bb01a
James Turner <mail.jt@gmail.com>
2017-02-13 14:57:44 +0000

    added short versions of notes

--- src/note.js
+++ src/note.js
@@ -3,3 +3,11 @@
 function Note(content) {
   this.content = content;
 }
+
+Note.prototype.short = function () {
+  if(this.content.length > 20) {
+    return this.content.substring(0,20)+"...";
+  }else{
+    return this.content
+  }
+};
