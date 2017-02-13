c4534cc098170a3223dd0d974d1bd3ab9765af32
Rob Brentnall <rob.brentnall@gmail.com>
2017-02-13 16:41:07 +0000

    fade in and current notes section

--- src/interface.js
+++ src/interface.js
@@ -3,12 +3,45 @@ var list = new List
 
 document.getElementById('addnote').addEventListener('click', function(event) {
     list.createNote(document.getElementById('new-note-content').value);
+    document.getElementById('new-note-content').value = "";
     updateNotes();
+    fadeIn('notes-container');
 }, false);
 
 function updateNotes() {
     var index = (list.notes.length - 1);
-    var noteId = "<li id=note-" + index + ">";
-    var content = noteId + "<a href='URL-GOES-HERE'/>" + list.notes[index].short() + "</li>";
+    var noteId = "<li class=note id=note-" + index + ">";
+    var content = noteId + list.notes[index].short() + "</li>";
     document.getElementById('notes').innerHTML += content;
+    getNotes();
+}
+
+function getNotes() {
+    var notes = document.getElementsByClassName('note');
+    for(var i = 0; i <= (notes.length - 1); i++) {
+        noteClick(notes, i);
+    }
+}
+
+function noteClick(notes, i) {
+    document.getElementById(notes[i].id).addEventListener('click', function(event) {
+    var elem = document.getElementById('currentnote');
+    elem.innerHTML = list.notes[i].content;
+    fadeIn('current-note-container');
+    }, false);
+}
+
+function fadeIn(container) {
+    var elem = document.getElementById(container);
+    var opacity = 0;
+    var id = setInterval(frame, 12);
+    function frame() {
+        if (opacity >= 100) {
+            clearInterval(id);
+        } else {
+            opacity++;
+            document.getElementById(container).classList.add("show");
+            elem.style.opacity = (Math.floor(opacity) / 100);
+        }
+    }
 }
