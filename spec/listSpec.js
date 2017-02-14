1e1a807d129c31be7ca437f28584f824365dc69c
Benjamin Carson <benjohncarson@gmail.com>
2017-02-14 16:04:59 +0000

    tests for list

---
+++ spec/listSpec.js
@@ -0,0 +1,10 @@
+'use strict';
+
+describe("List", function() {
+  it("can store new notes", function(){
+    var list = new List;
+    list.createNote("message");
+    var message = list.notes[0].content;
+    return assert.Equals(message, "message");
+  });
+})
