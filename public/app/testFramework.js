3532da715ccde539b98bccbce4b99196b69c058f
Rob Brentnall <rob.brentnall@gmail.com>
2017-02-14 13:47:39 +0000

    test works but does not display on page due to iframe loading time

--- public/app/testFramework.js
+++ public/app/testFramework.js
@@ -1,63 +1,66 @@
 
 (function(exports){
 
+  document.write("<div id='testResults'>Pass = 0 Fail = 0</div>");
+  var pass = 0;
+  var fail = 0;
 
   function Equals(passFunction, result, answer="expected " + passFunction + " to equal " + result) {
     if (passFunction===result){return true;}else{return answer;}
-  };
+  }
 
   function NotEquals(passFunction, result, answer="expected " + passFunction + " not to equal " + result) {
     if (passFunction!==result){return true;}else{return answer;}
-  };
+  }
 
   function GreaterThan(passFunction, result, answer="expected " + passFunction + " to be greater than " + result) {
     if (passFunction > result){return true;}else{return answer;}
-  };
+  }
 
   function LessThan(passFunction, result, answer="expected " + passFunction + " to be less than " + result) {
     if (passFunction < result){return true;}else{return answer;}
-  };
+  }
 
   function Contains(passFunction, result, answer="expected " + passFunction + " to contain " + result) {
     if (passFunction.includes(result)){return true;}else{return answer;}
-  };
+  }
 
-  function HasContent (website, result, answer="WRONG"){
-    document.write("<iframe id='iframe01' height='100' width='100' src='src/test.htm'>");
-    var content = document.getElementById('iframe01').contentDocument.body;
-    document.write("</iframe>");
-    if (content.matches("THE")){return true;}else{return answer;}
-  };
+  function HasContent(website, result, answer = "WRONG") {
+    document.write("<iframe id='iframe01' height='100' width='100' src='src/testFrameworkSpec/test.htm'></iframe>");
+
+    document.getElementById('iframe01').onload = function() {
+        var content = document.getElementById('iframe01').contentWindow.document.getElementById('testing').innerHTML
+        var answer = content.match(result)[0];
+        if (answer === result ) {
+            return true;
+        } else {
+            return answer;
+        }
+    }
+  }
 
   function output (title, result) {
     var css = "";
+    var fail;
     if (result!==true){css="in"; fail++;}else{pass++;}
     var output = "<div id='"+css+"correct'>&nbsp&nbsp&nbsp&nbsp"+title+": "+result+"</div>";
     return output;
-  };
+  }
 
   function it(title, passFunction){
     var result = passFunction();
     document.write(output(title, result));
-  };
+  }
 
   function describe (title, passFunction) {
-    console.log("not called")
     document.write("<b>"+title+"</b>");
     passFunction();
     displayResult();
-  };
-
-  function initiate(){
-    document.write("<div id='testResults'>Pass = 0 Fail = 0</div>");
-    var pass = 0;
-    var fail = 0;
   }
 
   function displayResult(){
     document.write("<script>document.getElementById('testResults').innerHTML = 'Pass = "+ pass +" Fail = "+ fail +"'</script>");
-  };
-  initiate()
+  }
 
   exports.assert = {}
   exports.assert.Equals = Equals;
