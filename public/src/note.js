71ae2bdc0fa21a46b4c8491792129fa84b1a6b6e
Rob Brentnall <rob.brentnall@gmail.com>
2017-02-14 15:51:05 +0000

    finished

--- public/src/note.js
+++ public/src/note.js
@@ -1,11 +1,11 @@
 'use strict';
 
 (function(exports){
-  
+
   function Note(content){
     this.content = content;
   }
-  
+
   Note.prototype.short = function() {
     if(this.content.length > 20) {
       return this.content.substring(0,20)+"...";
@@ -13,7 +13,7 @@
       return this.content;
     }
   };
-  
+
   exports.Note = Note;
   exports.Note.short = Note.short;
 })(this);
