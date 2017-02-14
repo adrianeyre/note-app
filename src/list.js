1f61a8ecf8cc2741d3e98110d8abfae15db19e1f
Joe Marriott <marriott90@gmail.com>
2017-02-14 13:04:06 +0000

    started refactoring to modular pattern

--- src/list.js
+++ src/list.js
@@ -1,10 +1,14 @@
 'use strict';
 
-function List() {
-  this.notes = [];
-}
 
-List.prototype.createNote = function(content) {
-  var note = new Note(content);
-  this.notes.push(note);
-};
+(function(exports){
+  function List(notes) {
+    this.notes = [];
+  }
+  List.prototype.createNote(content){
+    var note = new Note(content);
+    List.notes.push(note);
+  }
+  exports.List = List;
+  exports.List.createNote = List.createNote;
+})(this);
--- src/note.js
+++ src/note.js
@@ -1,13 +1,16 @@
 'use strict';
 
-function Note(content) {
-  this.content = content;
-}
-
-Note.prototype.short = function () {
-  if(this.content.length > 20) {
-    return this.content.substring(0,20)+"...";
-  }else{
-    return this.content
+(function(exports){
+  function Note(content){
+    this.content = content;
   }
-};
+  Note.protoype.short(this.content) {
+    if(this.content.length > 20) {
+      return this.content.substring(0,20)+"...";
+    }else{
+      return this.content
+    }
+  }
+ exports.Note.short = Note.short;
+ exports.Note = Note;
+})(this);
